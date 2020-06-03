3f4e6084e529e6fd4694c118b8edff03655d13a8
lauren <ljdutton2@icloud.com>
2020-06-03 14:21:08 -0700

    users create events

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 
 // require handlebars
-var exphbs = require('express-handlebars');
+
 
 // Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
@@ -16,12 +16,37 @@ var events = [
     { title: "I am your second event", desc: "A great event that is super fun to look at and good", img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
   ]
+    // INITIALIZE BODY-PARSER AND ADD IT TO APP
+  const bodyParser = require('body-parser');
+
+  const models = require('./db/models');
+  const handlebars = require('handlebars');
+
+  const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+  const exphbs = require('express-handlebars');
+  const hbs = exphbs.create({
+      defaultLayout: 'main',
+      handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+  app.engine('handlebars', hbs.engine);
+  // The following line must appear AFTER const app = express() and before your routes!
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+    // CREATE
+  app.post('/events', (req, res) => {
+    models.Event.create(req.body).then(event => {
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err)
+    });
+  })
   
-  // INDEX
-  app.get('/', (req, res) => {
+  // Index
+app.get('/', (req, res) => {
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
     res.render('events-index', { events: events });
   })
-
+})
   // NEW
   app.get('/events/new', (req, res) => {
     res.render('events-new', {});
@@ -33,3 +58,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App listening on port 3000!')
 })
+
+
