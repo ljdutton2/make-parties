0ead01ec1c3c254e03f8a3192e5f3df2e66f1125
lauren <ljdutton2@icloud.com>
2020-06-10 15:10:57 -0700

    proper seperation of concerns

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'))
 
-// require handlebars
+
 
 
 // Use "main" as our default layout
@@ -36,66 +36,6 @@ var events = [
   // The following line must appear AFTER const app = express() and before your routes!
   app.use(bodyParser.urlencoded({ extended: true }));
 
-    // CREATE
-  app.post('/events', (req, res) => {
-    models.Event.create(req.body).then(event => {
-      // Redirect to events/:id
-    res.redirect(`/events/${event.id}`)
-    }).catch((err) => {
-      console.log(err)
-    });
-  })
-  
-  // Index
-app.get('/', (req, res) => {
-  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-    res.render('events-index', { events: events });
-  })
-})
-  // NEW
-  app.get('/events/new', (req, res) => {
-    res.render('events-new', {});
-})
-  //SHOW
-  app.get('/events/:id', (req, res) => {
-    // Search for the event by its id that was passed in via req.params
-    models.Event.findByPk(req.params.id).then((event) => {
-      // If the id is for a valid event, show it
-      res.render('events-show', { event: event })
-    }).catch((err) => {
-      // if they id was for an event not in our db, log an error
-      console.log(err.message);
-    })
-  });
-  // EDIT
-  app.get('/events/:id/edit', (req, res) => {
-    models.Event.findByPk(req.params.id).then((event) => {
-      res.render('events-edit', { event: event });
-    }).catch((err) => {
-      console.log(err.message);
-  })
-});
-  // UPDATE
-  app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-  // DELETE
-  app.delete('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id).then(event => {
-      event.destroy();
-      res.redirect(`/`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  })
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
@@ -104,4 +44,4 @@ app.listen(port, () => {
   console.log('App listening on port 3000!')
 })
 
-
+require('./controllers/events')(app, models);
