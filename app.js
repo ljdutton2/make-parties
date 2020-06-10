88b84cfa6810a1c64cc3bf1fd731d39db1ee91cd
lauren <ljdutton2@icloud.com>
2020-06-10 14:52:39 -0700

    delete

--- app.js
+++ app.js
@@ -87,6 +87,15 @@ app.get('/', (req, res) => {
     console.log(err);
   });
 });
+  // DELETE
+  app.delete('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id).then(event => {
+      event.destroy();
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  })
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
